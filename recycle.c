ca2506a81a8ba50eaaeb0159e7ce32e732729913
Matt Robinson <git@nerdoftheherd.com>
2024-06-10 19:17:52 +0100

    Add kprobe hook to security_path_unlink
    Unfortunately, Linux Security Modules have to be compiled in to the
    kernel so we can't register ourselves as one and get called at unlink
    that way.  Instead, register a kprobe against security_path_unlink and
    just log a message to show that our function is being called.

--- recycle.c
+++ recycle.c
@@ -6,18 +6,39 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/kprobes.h>
 #include <linux/module.h>
 
 MODULE_LICENSE("GPL");
 
+int pre_security_path_unlink(struct kprobe *p, struct pt_regs *regs)
+{
+    pr_info("Before security_path_unlink\n");
+    return 0;
+}
+
+static struct kprobe kp = {
+    .symbol_name = "security_path_unlink",
+    .pre_handler = pre_security_path_unlink,
+};
+
 static __init int recycle_init(void)
 {
+    int error = register_kprobe(&kp);
+
+    if(error)
+    {
+        pr_err("Failed to register kprobe for %s\n", kp.symbol_name);
+        return error;
+    }
+
     pr_info("Init complete\n");
     return 0;
 }
 
 static __exit void recycle_exit(void)
 {
+    unregister_kprobe(&kp);
     pr_info("Exiting\n");
 }
 
